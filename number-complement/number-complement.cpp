53723f2d0bb4b48ccbf77cf860b522c792353323
Sachin <66303835+Sachindebug@users.noreply.github.com>
2021-12-27 15:34:43 +0530

    Time: 0 ms (100.00%), Space: 6 MB (40.00%) - LeetHub

---
+++ number-complement/number-complement.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findComplement(int n) {
+        if(n==0) return 1;
+        long long int res=setBitNumber(n);
+        res+=res;
+        res-=1;
+        cout<<res<<endl;
+        return res^n;
+        
+    }
+
+    long long int setBitNumber(long long int n)
+    {
+    if (n == 0)
+        return 0;
+ 
+    long long int msb = 0;
+    n = n / 2;
+    while (n != 0) {
+        n = n / 2;
+        msb++;
+    }
+ 
+    return (1 << msb);
+    }
+};
