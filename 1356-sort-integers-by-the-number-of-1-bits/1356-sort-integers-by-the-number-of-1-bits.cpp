6a26fa3f2f1301eb5f63ff1c0881ad68787de909
Sachin <66303835+Sachindebug@users.noreply.github.com>
2022-01-11 12:06:03 +0530

    Time: 22 ms (14.73%), Space: 10.1 MB (64.37%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,36 +1,17 @@
 class Solution {
 public:
     vector<int> sortByBits(vector<int>& arr) {
-        vector<pair<int,int>> temp;
-        for(auto x:arr)
-        {
-            int c=countBits(x);
-            temp.push_back({x,c});
-        }
-        sort(temp.begin(),temp.end(),myfun);
-        vector<int> res;
-        for(auto x:temp)
-        {
-            res.push_back(x.first);
-        }
-        return res;
-    }
-    int countBits(int n)
-    {
-        int c=0;
-        while(n>0)
-        {
-            
-            n=n&n-1;
-            c++;
-        }
-        return c;
+        sort(arr.begin(),arr.end(),myfun);
+        return arr;
     }
     
-    static bool myfun(pair<int,int> p1,pair<int,int> p2)
+    static bool myfun(int a,int b)
     {
-        if(p1.second<p2.second) return true;
-        if(p1.second==p2.second && p1.first<p2.first) return true;
+        int c1=__builtin_popcount(a);
+        int c2=__builtin_popcount(b);
+        if(c1<c2) return true;
+        if(c1==c2 && a<b) return true;
         return false;
+
     }
 };
