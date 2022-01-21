4da75f3ce17e4094b558ec7299145f7478ec6efd
Sachin <66303835+Sachindebug@users.noreply.github.com>
2022-01-21 20:42:22 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (63.74%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,47 +1,14 @@
 class Solution {
 public:
-    bool reorderedPowerOf2(int n) {
-        vector<int> arr;
-        int p=n;
-        while(p>0)
-        {
-            arr.push_back(p%10);
-            p/=10;
-        }
-        vector<int> res=allpermutation(arr);
-        
-        
-        for(int i=0;i<res.size();i++)
-        {
-            if(floor(log2(res[i]))==ceil(log2(res[i]))) return true;
-        }
-        return false;
-        
+    bool reorderedPowerOf2(int N) {
+    string n = to_string(N);
+	sort(begin(n), end(n));
+	for(int i = 0, powOf2 = 1; i < 30; i++, powOf2 <<= 1){
+		string pow2_str = to_string(powOf2);
+		sort(begin(pow2_str), end(pow2_str));
+		if(n == pow2_str) return true;  
+	}
+	return false;
     }
-    vector<int> allpermutation(vector<int> &arr)
-    {
-        sort(arr.begin(),arr.end());
-        vector<int> res;
-        do{
-            if(arr[0]!=0)
-            {
-                vector<int> temp=arr;
-                int p=1;
-                int a=0;
-                for(int i=temp.size()-1;i>=0;i--)
-                {
-                    a+=(temp[i]*p);
-                    p*=10;
-                }
-                res.push_back(a);
-            }
-            
-        }
-        while(next_permutation(arr.begin(),arr.end()));
-        
-        
-        
-        
-        return res;
-    }
+    
 };
