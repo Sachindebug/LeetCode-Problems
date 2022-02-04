6e26b0181e608bb14a9fbdb35aa610318b0cc4ec
Sachin <66303835+Sachindebug@users.noreply.github.com>
2022-02-04 10:50:20 +0530

    Time: 257 ms (16.08%), Space: 83.6 MB (98.62%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -1,32 +1,14 @@
 class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
-        int n=nums.size();
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]==0)
-            {
-                nums[i]=-1;
-            }
-        }
-        unordered_map<int,vector<int>> ump;
-        int sum=0;
-        for(int i=0;i<n;i++)
-        {
-            sum+=nums[i];
-            ump[sum].push_back(i);
-        }
-        int res=INT_MIN;
-        for(auto x:ump)
-        {
-            int s=x.first;
-            if(s==0) res=max(res,x.second[x.second.size()-1]+1);
-            else
-            {
-                res=max(res,x.second[x.second.size()-1]-x.second[0]);
-            }
-        }
+        int sum=0, maxLen=0;
+        unordered_map<int, int> seen{{0, -1}};
         
-        return res;
+        for(int i=0; i<nums.size(); i++){
+            sum += nums[i]==1 ? 1 : -1;
+            if(seen.count(sum)) maxLen = max(maxLen, i-seen[sum]);
+            else seen[sum] = i;
+        }
+        return maxLen;
     }
 };
