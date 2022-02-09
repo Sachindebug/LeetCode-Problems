5ec9b4ecb450559f9a81c14ba38203ab43b350f5
Sachin <66303835+Sachindebug@users.noreply.github.com>
2022-02-09 10:17:49 +0530

    Time: 34 ms (34.77%), Space: 13.2 MB (58.82%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -3,19 +3,27 @@ public:
     int findPairs(vector<int>& nums, int k) {
         set<pair<int,int>> s;
         int n=nums.size();
-        for(int i=0;i<n;i++)
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n-1;i++)
         {
-            for(int j=i+1;j<n;j++)
+            int x=k+nums[i];
+            int y=nums[i]-k;
+            if(binary_search(nums.begin()+i+1,nums.end(),x))
             {
-                if(abs(nums[i]-nums[j])==k)
-                {
-                    int x=min(nums[i],nums[j]);
-                    int y=max(nums[i],nums[j]);
-                    pair<int,int> p={x,y};
-                    s.insert(p);
-
-                }
+                int a=min(nums[i],x);
+                int b=max(nums[i],x);
+                pair<int,int> p={a,b};
+                s.insert(p);
             }
+            if(binary_search(nums.begin()+i+1,nums.end(),y))
+            {
+                int a=min(nums[i],y);
+                int b=max(nums[i],y);
+                pair<int,int> p={a,b};
+                s.insert(p);
+            }
+            
+            
         }
         return s.size();
     }
