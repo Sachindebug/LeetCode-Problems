e0fee9c883c7af689944cab6f9c8698cdbbd8238
Sachin <66303835+Sachindebug@users.noreply.github.com>
2022-02-23 13:09:31 +0530

    Time: 12 ms (25.16%), Space: 16.2 MB (8.65%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -12,52 +12,19 @@
 class Solution {
 public:
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        vector<int> inorder=preorder;
-        sort(inorder.begin(),inorder.end());
-        return buildTree(preorder,inorder);
-    }
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int n=preorder.size();
-        if(n==0) return nullptr;
-        TreeNode *root=new TreeNode(preorder[0]);
-        if(n==1)
-        {
-            return root;
-        }
-        int idx=0;
-        int x=preorder[0];
-        for(int i=0;i<n;i++)
-        {
-            if(inorder[i]==x)
-            {
-                idx=i;
-                break;
-            }
-        }
+        if(preorder.size()==0) return nullptr;
+        if(preorder.size()==1) return new TreeNode(preorder[0]);
         vector<int> p1;
-        vector<int> i1;
         vector<int> p2;
-        vector<int> i2;
-        
-        for(int i=0;i<idx;i++)
+        TreeNode* root=new TreeNode(preorder[0]);
+        for(int i=1;i<preorder.size();i++)
         {
-            i1.push_back(inorder[i]);
+            if(preorder[i]>preorder[0]) p2.push_back(preorder[i]);
+            else p1.push_back(preorder[i]);
         }
-        for(int i=idx+1;i<n;i++)
-        {
-            i2.push_back(inorder[i]);
-        }
-        int sz=i1.size();
-        for(int i=1;i<n && i<=sz;i++)
-        {
-            p1.push_back(preorder[i]);
-        }
-        for(int i=sz+1;i<n;i++)
-        {
-            p2.push_back(preorder[i]);
-        }
-        root->left=buildTree(p1,i1);
-        root->right=buildTree(p2,i2);
+        root->left=bstFromPreorder(p1);
+        root->right=bstFromPreorder(p2);
         return root;
     }
+
 };
