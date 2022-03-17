4f789741986f73ba4257fd6a19755e8255164f26
Sachin <66303835+Sachindebug@users.noreply.github.com>
2022-03-17 11:20:19 +0530

    Time: 2 ms (57.21%), Space: 11.4 MB (6.20%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
-        if(n==1) return nums[0];
+        if(n==01) return nums[0];
         vector<int> dp1(n+2,-1);
         vector<int> dp2(n+2,-1);
         return max(recurse(nums,0,n-1,dp1),recurse(nums,1,n,dp2));
