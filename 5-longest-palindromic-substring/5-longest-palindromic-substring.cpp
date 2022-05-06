df80708fb3c1070e9599f3a4764deff31cbcf9f6
Sachin <66303835+Sachindebug@users.noreply.github.com>
2022-05-06 11:37:49 +0530

    Time: 1164 ms (6.74%), Space: 30.1 MB (33.45%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -8,16 +8,16 @@ public:
          for (int i = 0; i < n - 1; ++i) dp[i][i + 1] = s[i] == s[i + 1];
          for (int i = n - 3; i >= 0; --i) {
          for (int j = i + 2; j < n; ++j) {
-         dp[i][j] = dp[i + 1][j - 1] && s[i] == s[j];
-         }
+                dp[i][j] = dp[i + 1][j - 1] && s[i] == s[j];
+            }
          }
          for (int i = 0; i < n; ++i) {
          for (int j = i; j < n; ++j) {
          if (dp[i][j] && j - i + 1 > max_len) {
-         max_len = j - i + 1;
-         start = i;
-         }
-         }
+                 max_len = j - i + 1;
+                 start = i;
+            }
+          }
          }
          return s.substr(start, max_len);
  } 
