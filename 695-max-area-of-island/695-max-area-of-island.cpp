65c2296f38efb80c5a9ee0076e850c2d384e278c
Sachin <66303835+Sachindebug@users.noreply.github.com>
2022-06-02 16:36:16 +0530

    Time: 38 ms (23.44%), Space: 23.9 MB (30.97%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -3,32 +3,32 @@ public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int n=grid.size(),m=grid[0].size();
         int count=0;
-        
+        vector<vector<int>> seen(n,vector<int>(m,0));
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(grid[i][j]==1)
+                if(grid[i][j] && !seen[i][j])
                 {
-                    
-                    int res=dfsislands(grid,i,j);
+                    int res=dfsislands(grid,i,j,seen);
                     count=max(res,count);
                 }
             }
         }
         return count;
     }
-    int  dfsislands(vector<vector<int>>& grid, int i, int j)
+    int  dfsislands(vector<vector<int>>& grid, int i, int j,vector<vector<int>>& seen)
     {
         
-        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]!=1)
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || !grid[i][j])
         {
             return 0;
         }
-        
+        seen[i][j]=1;
         grid[i][j]=0;
         
-        return 1+dfsislands(grid,i+1,j)+dfsislands(grid,i,j+1)+dfsislands(grid,i-1,j)+dfsislands(grid,i,j-1);
+        return 1+dfsislands(grid,i+1,j,seen)+dfsislands(grid,i,j+1,seen)+
+                 dfsislands(grid,i-1,j,seen)+dfsislands(grid,i,j-1,seen);
     }
     
    
