27ea0a6f74f02884b5f6a9fdbcaa9412a40610ce
Sachin <66303835+Sachindebug@users.noreply.github.com>
2022-07-14 14:10:18 +0530

    Time: 30 ms (54.31%), Space: 23.9 MB (29.56%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -20,12 +20,12 @@ public:
     int  dfsislands(vector<vector<int>>& grid, int i, int j,vector<vector<int>>& seen)
     {
         
-        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || !grid[i][j])
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || !grid[i][j] || seen[i][j])
         {
             return 0;
         }
         seen[i][j]=1;
-        grid[i][j]=0;
+        
         
         return 1+dfsislands(grid,i+1,j,seen)+dfsislands(grid,i,j+1,seen)+
                  dfsislands(grid,i-1,j,seen)+dfsislands(grid,i,j-1,seen);
