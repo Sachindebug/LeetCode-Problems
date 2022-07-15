ec9bdcbecc8fc39fc21efdfe3a7332a300a8ba0b
Sachin <66303835+Sachindebug@users.noreply.github.com>
2022-07-15 16:45:14 +0530

    Time: 110 ms (45.95%), Space: 61.7 MB (27.14%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
-    bool increasingTriplet(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> left(n);
-        left[0]=nums[0];
-        int mn=left[0];
-        for(int i=1;i<n;i++)
+    bool increasingTriplet(vector<int>& arr) {
+        int n=arr.size();
+        int low=INT_MAX;
+        int mid=INT_MAX;
+        for(int i=0;i<n;i++)
         {
-            mn=min(mn,nums[i]);
-            left[i]=mn;
+            if(arr[i]<=low) low=arr[i];
+            else if(arr[i]<=mid) mid=arr[i];
+            else return true;
         }
-        vector<int> right(n);
-        right[n-1]=nums[n-1];
-        int mx=nums[n-1];
-        for(int i=n-2;i>=0;i--)
-        {
-            mx=max(mx,nums[i]);
-            right[i]=mx;
-        }
-        for(int i=1;i<n-1;i++)
-        {
-            if(nums[i]>left[i] && nums[i]<right[i]) return true;
-        }
+        
         return false;
         
     }
