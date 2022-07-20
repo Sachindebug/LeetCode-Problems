be4a755e098cf73e84cffff02ab1dd94c097077f
Sachin <66303835+Sachindebug@users.noreply.github.com>
2022-07-20 09:00:30 +0530

    Time: 260 ms (74.40%), Space: 38 MB (85.02%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -3,8 +3,8 @@ public:
     int numMatchingSubseq(string s, vector<string>& words) {
         int res=0;
         int count=0,k=s.size();
-      //we will be form map to count occurences of particular word to avoid re calculation and this will reduce time
-      unordered_map<string,int>mymap;
+   
+      unordered_map<string,int> mymap;
       for(int i=0;i<words.size();i++)
         mymap[words[i]]++;
       for(auto x:mymap)
@@ -19,7 +19,7 @@ public:
           else
             start1++;
         }if(start2==m)
-          count+=x.second;//if we reach the length of string p then this word in in our string s. so we increase our count by number of times it occured in words instead of increamenting by 1
+          count+=x.second;
       }return count;
     }
 };
