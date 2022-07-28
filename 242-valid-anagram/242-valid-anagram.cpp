147e54bf00d29e7cc27d0fc6f39ef8ddfd7d1ef8
Sachin <66303835+Sachindebug@users.noreply.github.com>
2022-07-28 09:17:05 +0530

    Time: 6 ms (90.64%), Space: 7.3 MB (77.97%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> arr(26,0);
+        for(auto x:s) arr[x-'a']++;
+        for(auto x:t) arr[x-'a']--;
+        for(auto x:arr) if(x!=0) return false;
+        return true;
+    }
+};
