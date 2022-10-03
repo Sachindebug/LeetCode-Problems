ed7875d534ae5c8518fc8dafe4489fd4ec89ec69
Sachin <66303835+Sachindebug@users.noreply.github.com>
2022-10-03 20:38:18 +0530

    Time: 51 ms (99.25%), Space: 73.1 MB (30.10%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
     int longestPalindromeSubseq(string s) {
-        string t=s;
-        reverse(t.begin(),t.end());
-        return longestCommonSubsequence(s,t);
+        int n = s.size();
+        vector<vector<int>> mem(n,vector<int>(n));
+        return longestPalindromeSubseq(0,n-1, s,mem); 
     }
-    int longestCommonSubsequence(string text1, string text2) {
-        int n=text1.length();
-        int m=text2.length();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=m;j++)
-            {
-                if(text1[i-1]==text2[j-1])
-                {
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
-                else
-                {
-                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                
-                }
-            }
-        }
-        return dp[n][m];
+    int longestPalindromeSubseq(int l, int r, string &s, vector<vector<int>>& mem) {
+        if(l==r) return 1;
+        if(l>r) return 0;
+        if(mem[l][r]) return mem[l][r];
+        return mem[l][r] = s[l]==s[r] ? 2 + longestPalindromeSubseq(l+1,r-1, s,mem) : 
+            max(longestPalindromeSubseq(l+1,r, s,mem),longestPalindromeSubseq(l,r-1, s,mem)); 
+        
     }
 };
