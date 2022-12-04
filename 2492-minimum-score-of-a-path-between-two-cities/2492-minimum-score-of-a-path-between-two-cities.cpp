0a4778fa137083f7e5e94bbf74acfaa473d4dc8b
Sachin <66303835+Sachindebug@users.noreply.github.com>
2022-12-04 14:40:42 +0530

    Time: 1352 ms (9.52%), Space: 158.9 MB (9.52%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
     int minScore(int n, vector<vector<int>>& roads) {
-        unordered_map<int,vector<pair<int,int>> > adj;
-        for(auto p:roads){
-            int u=p[0];
-            int v=p[1];
-            int wt=p[2];
-            adj[u].push_back({v,wt});
-            adj[v].push_back({u,wt});
+        int res=INT_MAX;
+        unordered_map<int,vector<pair<int,int>>> ump;
+        for(auto x:roads)
+        {
+            int u=x[0];
+            int v=x[1];
+            int w=x[2];
+            ump[u].push_back({v,w});
+            ump[v].push_back({u,w});
         }
-        int mindist=INT_MAX;
-        vector<int> vis(n+1,0);
-        dfs(1,n,adj,mindist,vis);
-        return mindist;
+        unordered_map<int,int> vis;
+        dfs(ump,vis,res,1);
+        return res;
     }
-    void dfs(int u,int n,unordered_map<int,vector<pair<int,int>> > &adj, int &mindist,vector<int> &vis){
-        vis[u]=1;
-        for(auto p:adj[u]){
-            int v=p.first;
-            int wt=p.second;
-            mindist=min(mindist, wt);
-            if(!vis[v]){
-                dfs(v,n,adj, mindist,vis);
+    void dfs(unordered_map<int,vector<pair<int,int>>> &ump,unordered_map<int,int> &vis,int &res,int s)
+    {
+        vis[s]=1;
+        for(auto x:ump[s])
+        {
+            res=min(res,x.second);
+            if(!vis[x.first])
+            {
+                
+                dfs(ump,vis,res,x.first);
             }
         }
     }
