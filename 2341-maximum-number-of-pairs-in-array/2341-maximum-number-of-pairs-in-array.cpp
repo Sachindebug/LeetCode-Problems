025b22b77acee5153b9f3736a974617515c0eca9
Sachin <66303835+Sachindebug@users.noreply.github.com>
2022-12-20 14:18:30 +0530

    Time: 4 ms (67.20%), Space: 9.4 MB (32.31%) - LeetHub

---
+++ 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> numberOfPairs(vector<int>& nums) {
+        map<int,int>mp;
+        int count=0;
+        int rem=0;
+        for(auto i : nums)
+            mp[i]++;
+        for(auto i : mp){
+            count+=i.second/2;
+            rem+=i.second%2;
+        }
+        nums.resize(2);
+        nums.pop_back();
+        nums.pop_back();
+
+        nums.push_back(count);
+        nums.push_back(rem);
+        return nums;
+    }
+};
