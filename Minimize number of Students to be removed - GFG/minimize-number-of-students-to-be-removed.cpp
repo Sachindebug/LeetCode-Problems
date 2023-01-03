31ac2ab95cc188071329f715070d2415e06a0e46
Sachin <66303835+Sachindebug@users.noreply.github.com>
2023-01-03 10:29:07 +0530

    Added solution - LeetHub

---
+++ Minimize number of Students to be removed - GFG/minimize-number-of-students-to-be-removed.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int lengthOfLIS(int nums[],int n){
+        vector<int> res;
+        for(int i=0; i<n; i++) {
+            auto it = std::lower_bound(res.begin(), res.end(), nums[i]);
+            if(it==res.end()) res.push_back(nums[i]);
+            else *it = nums[i];
+        }
+        return res.size();
+    }
+    int removeStudents(int H[], int N) {
+        // code here
+        return N-lengthOfLIS(H,N);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin>>N;
+        
+        int H[N];
+        for(int i=0; i<N; i++)
+            cin>>H[i];
+
+        Solution ob;
+        cout << ob.removeStudents(H,N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
