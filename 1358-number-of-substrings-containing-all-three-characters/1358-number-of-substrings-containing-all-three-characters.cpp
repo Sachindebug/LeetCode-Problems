fae380974582fba42d9bb683c482fb8fb9ca938a
Sachin <66303835+Sachindebug@users.noreply.github.com>
2023-03-15 01:11:01 +0530

    Time: 16 ms (91.56%), Space: 8.5 MB (60.44%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -8,9 +8,9 @@ public:
         {
             if(s[i] == 'a')
                 a++;
-            if(s[i] == 'b')
+            else if(s[i] == 'b')
                 b++;
-            if(s[i] == 'c')
+            else
                 c++;
 
             if(a > 0 && b > 0 && c > 0)
