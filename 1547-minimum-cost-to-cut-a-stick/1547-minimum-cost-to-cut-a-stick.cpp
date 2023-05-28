2c54a3883cefec6fc0ffd17421f06475cdece29f
Sachin <66303835+Sachindebug@users.noreply.github.com>
2023-05-28 12:34:54 +0530

    Time: 61 ms (81.09%), Space: 10.2 MB (60.72%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    int minCost(int n, vector<int>& cuts) {
+    int minCost(int n, vector<int>& arr) {
         int i=1;
-        int j=cuts.size();
-        int m=cuts.size();
-        vector<vector<int>> dp(m+1,vector<int>(m+1,-1));
-        cuts.push_back(n);
-        cuts.insert(cuts.begin(),0);
-        sort(cuts.begin(),cuts.end());
-        return recurse(cuts,1,j,dp);
-    }
-    int recurse(vector<int> &arr,int i,int j,vector<vector<int>> &dp)
-    {
-        if(i>j) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int mini=INT_MAX;
-        for(int k=i;k<=j;k++)
+        int j=arr.size();
+        int m=j;
+        arr.push_back(n);
+        arr.insert(arr.begin(),0);
+        sort(arr.begin(),arr.end());
+        vector<vector<int>> dp(m+2,vector<int>(m+2,0));
+        for(int i=m;i>0;i--)
         {
-            int cost = arr[j+1]-arr[i-1]+recurse(arr,i,k-1,dp)+recurse(arr,k+1,j,dp);
-            mini=min(mini,cost);
+            for(int j=1;j<=m;j++)
+            {
+                if(i>j) continue;
+                int mini=INT_MAX;
+                for(int k=i;k<=j;k++)
+                {
+                    int cost = arr[j+1]-arr[i-1]+dp[i][k-1]+dp[k+1][j];
+                    mini=min(mini,cost);
+                }
+                dp[i][j]=mini;
+                
+            }
         }
-        return dp[i][j]=mini;
+        return dp[1][m];
     }
+    
 };
