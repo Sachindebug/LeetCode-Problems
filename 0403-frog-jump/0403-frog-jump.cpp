eab358448729743df0a43d09bf98c911ad2adc38
Sachin <66303835+Sachindebug@users.noreply.github.com>
2023-08-27 13:10:52 +0530

    Time: 90 ms (78.45%), Space: 213.2 MB (27.07%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -13,7 +13,7 @@ public:
             y=jump(ump[stones[i]+k],k,n,stones,dp);
         if(ump.find(stones[i]+k+1) != ump.end())
             z=jump(ump[stones[i]+k+1],k+1,n,stones,dp);
-        return dp[i][k]= (x || y || z);
+        return dp[i][k]= x || y || z;
     }
     bool canCross(vector<int>& stones) {
         if(stones[1]-stones[0]!=1)
@@ -23,7 +23,7 @@ public:
             return 1;
         for(int i=0; i<n; i++)
             ump[stones[i]]=i;
-        vector<vector<int>>dp(2000,vector<int>(2001,-1));
+        vector<vector<int>>dp(n,vector<int>(n+1,-1));
         return jump(1,1,n,stones,dp);
     }
 };
