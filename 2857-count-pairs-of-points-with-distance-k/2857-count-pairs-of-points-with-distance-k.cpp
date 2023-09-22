f78c32242a09aec6aaaf9d239ce02c0eb0f8ce1a
Sachin <66303835+Sachindebug@users.noreply.github.com>
2023-09-23 00:52:41 +0530

    Time: 396 ms (81.00%), Space: 89.4 MB (78.30%) - LeetHub

--- 2857-count-pairs-of-points-with-distance-k/2857-count-pairs-of-points-with-distance-k.cpp
+++ 2857-count-pairs-of-points-with-distance-k/2857-count-pairs-of-points-with-distance-k.cpp
@@ -1,36 +1,29 @@
 class Solution {
 public:
-    int countPairs(vector<vector<int>>& arr, int k) {
-        unordered_map<string,long long int> ump;
-        for(auto x:arr)
+    int countPairs(vector<vector<int>>& c, int k) {
+        int n = c.size();
+        int cx, cy, minx, maxx;
+        int res = 0;
+        map<pair<int,int>,int> mp;
+        for(auto p : c)
         {
-            string a = to_string(x[0]);
-            string b = to_string(x[1]);
-            string t = a+"+"+b;
-            ump[t]++;
+            mp[{p[0],p[1]}]++;
         }
-        long long int res=0;
-        for(int i=0;i<=k;i++)
+        for(int i = 0; i < n; i++)
         {
-            int x = i;
-            int y = k-i;
-            for(int j=0;j<arr.size();j++)
+            cx = c[i][0];
+            cy = c[i][1];
+            mp[{cx,cy}]--;
+            for(int i = 0; i <= k; i++)
             {
-                int a = arr[j][0] ^ x;
-                int b = arr[j][1] ^ y;
-                string s = to_string(a)+"+"+to_string(b);
-                int c = arr[j][0];
-                int d = arr[j][1];
-                if(a==c && b==d)
+                int x = cx^i;
+                int y = (k-i)^cy;
+                if(mp.find({x,y}) != mp.end())
                 {
-                    res+=((ump[s]-1));
+                    res+=mp[{x,y}];
                 }
-                else
-                    res+=ump[s];
             }
-            
         }
-        
-        return res/2;
+        return res;
     }
 };
