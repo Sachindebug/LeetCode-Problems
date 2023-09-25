1e4caf0c5399b4f6648f6b6ff3febee420acb7de
Sachin <66303835+Sachindebug@users.noreply.github.com>
2023-09-25 12:25:59 +0530

    Time: 4 ms (48.11%), Space: 6.9 MB (16.46%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char res=0;
+        for(int i=0;i<s.length();i++)
+        {
+            res=res^s[i];
+        }
+        for(int i=0;i<t.length();i++)
+        {
+            res=res^t[i];
+        }
+        return res;
+    }
+};
