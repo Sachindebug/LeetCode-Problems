a49c2c006df1cc41fe64d9938f8832bca96f7d43
Sachin <66303835+Sachindebug@users.noreply.github.com>
2023-09-28 12:04:01 +0530

    Time: 2 ms (44.73%), Space: 6.3 MB (14.13%) - LeetHub

--- 0880-decoded-string-at-index/0880-decoded-string-at-index.cpp
+++ 0880-decoded-string-at-index/0880-decoded-string-at-index.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
     string decodeAtIndex(string s, int k,long long len =0) {
-        for (auto i = 0; i < s.size(); ++i) {
-                if (isalpha(s[i])) {
-                    if (++len == k) return string(1, s[i]);
-
-                }
-                else {
-                    if (len * (s[i] - '0') >= k) return decodeAtIndex(s, k % len == 0 ? len : k % len);
-                    len *= s[i] - '0';
-                }
+        long long totalSize = 0; // To keep track of the total size of the decoded string.
+        
+        // Calculate the total size of the decoded string.
+        for (char c : s) {
+            if (isdigit(c)) {
+                int digit = c - '0';
+                totalSize *= digit;
+            } else {
+                totalSize++;
             }
-            
-            return 0;
+        }
+        
+        // Decode the string in reverse order.
+        for (int i = s.size() - 1; i >= 0; i--) {
+            char currentChar = s[i];
+            if (isdigit(currentChar)) {
+                int digit = currentChar - '0';
+                totalSize /= digit;
+                k %= totalSize;
+            } else {
+                if (k == 0 || k == totalSize) {
+                    return string(1, currentChar); // Found the character at k.
+                }
+                totalSize--; // Decrease the size for a single character.
+            }
+        }
+        
+        return ""; // This line should never be reached.
     }
 };
