c9f0af7d75c4d416cd99e71a8289167b0b778e5b
Sachin <66303835+Sachindebug@users.noreply.github.com>
2023-09-30 00:54:20 +0530

    Time: 128 ms (39.09%), Space: 96.9 MB (9.17%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.cpp
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -4,8 +4,7 @@ public:
         int n=nums.size();
         bool inc= nums[0]<nums[n-1]? 1 : 0;
         for(int i=0; i<n-1; i++){
-          cout<<inc<<endl;
-          cout<<i<<nums[i]<<nums[i+1];
+          
           if(inc==1 && nums[i]>nums[i+1])
             return 0;
           
