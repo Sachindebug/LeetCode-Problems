2cccb675c954a0ffdf78b398f12b2e8a1db645a0
Sachin <66303835+Sachindebug@users.noreply.github.com>
2024-05-19 12:25:59 +0530

    Time: 7 ms (80.00%), Space: 7.9 MB (100.00%) - LeetHub

---
+++ 3154-find-number-of-ways-to-reach-the-k-th-stair/3154-find-number-of-ways-to-reach-the-k-th-stair.cpp
@@ -0,0 +1,45 @@
+#define ll long long
+
+ll dp[50][50][2];
+
+ll find(int pos, int stat, int jump, int back, vector<ll> &exp, int k)
+{
+    if(pos > k+1)
+        return 0;
+    
+    if(dp[jump][back][stat] != -1)
+        return dp[jump][back][stat]; 
+    
+    ll ans = 0;
+    
+    if(pos == k)
+        ++ans; 
+
+    if(stat == 1)
+    {
+        ans += find(pos-1, 0, jump, back+1, exp, k);
+        ans += find(pos+exp[jump], 1, jump+1, back, exp, k);
+    }
+
+    if(stat == 0)
+        ans += find(pos+exp[jump], 1, jump+1, back, exp, k); // forward jump
+    
+    return dp[jump][back][stat] = ans;
+}
+
+
+class Solution {
+public:
+    int waysToReachStair(int k) {
+            
+        memset(dp, -1, sizeof(dp));
+        vector<ll> exp(33,0);
+        
+        for(int i=0; i<33; ++i)
+            exp[i] = pow(2, i);
+        
+        ll ans = find(1, 1, 0, 0, exp, k);  
+            
+        return ans;
+    }
+};
