629a0fe62f23078ce56df6ad04c95b2a83d0476a
Sachin <66303835+Sachindebug@users.noreply.github.com>
2024-05-28 12:02:54 +0530

    Time: 3 ms (97.07%), Space: 8.8 MB (37.19%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     int equalSubstring(string s, string t, int k) {
-        vector<int> arr;
-        for(int i=0;i<s.length();i++)
-        {
-            arr.push_back(abs(s[i]-t[i]));
-        }
         int res = 0;
         int sum = 0;
         int i = 0,j = 0;
-        while(j<arr.size())
+        while(j<s.size())
         {
-            sum+=arr[j];
+            sum+=abs(s[j]-t[j]);
             while(sum>k)
             {
-                sum-=arr[i];
+                sum-=abs(s[i]-t[i]);
                 i++;
             }
             res = max(res,j-i+1);
