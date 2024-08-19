0850304d15da62c37e0cfd040454cb1a45657a31
Sachin <66303835+Sachindebug@users.noreply.github.com>
2024-08-19 12:41:10 +0530

    Time: 0 ms (100%), Space: 7.5 MB (71.95%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int minSteps(int n) {
-        if(n==1) return 0;
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        return 1+recurse(1,1,n,dp);
-    }
-    int recurse(int added, int cop,int n,vector<vector<int>> &dp){
-        if(added>n) return 1000;
-        else if(added==n) return 0;
-        if(dp[added][cop]!=-1) return dp[added][cop];
-        return dp[added][cop] = min(1+recurse(added+cop,cop,n,dp),2+recurse(added*2,added,n,dp));
-    }
+       if(n==1) return 0;
+        for(int i = n-1;i>=1;i--){
+            if(n%i == 0)
+                return n/i + minSteps(i);
+        }
+        return 0;   
+    }   
 };
