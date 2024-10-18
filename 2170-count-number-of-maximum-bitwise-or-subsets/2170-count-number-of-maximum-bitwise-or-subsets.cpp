716dea0c5df7656650a11121b85b077b5c385aed
Sachin <66303835+Sachindebug@users.noreply.github.com>
2024-10-18 10:30:04 +0530

    Time: 7 ms (95.63%), Space: 10 MB (93.24%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -2,19 +2,12 @@ class Solution {
 public:
     int countMaxOrSubsets(vector<int>& nums) {
         int res=0;
-        for(auto x:nums)
-        {
-            res=res|x;
-        }
-        int count=0;
-        return recurse(nums,0,res,count);
+        for(auto x:nums) res=res|x;
+        return recurse(nums,0,res,0);
     }
     int recurse(vector<int> &nums,int idx,int x,int count)
     {
-        if(idx==nums.size())
-        {
-            return count==x;
-        }
+        if(idx==nums.size()) return count==x;
         return recurse(nums,idx+1,x,count)+recurse(nums,idx+1,x,count|nums[idx]);
     }
 };
