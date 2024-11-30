6c1cd94804ab4770a1be1f34066e86395548acd8
Sachin <66303835+Sachindebug@users.noreply.github.com>
2024-11-30 14:16:11 +0530

    Time: 3 ms (36.04%), Space: 13.3 MB (31.99%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int bits[32]={0};
-        for(int i=0;i<nums.size();i++)
-        {
-            for(int j=0;j<32;j++)
-            {
-                if(nums[i] & (1<<j))
-                {
-                    bits[j]++;
-                }
+        vector<int> bits(32,0);
+        for(int i = 0; i<32;i++){
+            for(auto x: nums){
+                if(x&(1<<i)) bits[i]++;
             }
         }
-        int ans=0;
-        for(int i=0;i<32;i++)
-        {
-            if(bits[i]%3!=0 && bits[i]!=0)
-            {
-                ans=ans|(1<<i);
-            }
-        }
-        return ans;
-        
+        int res = 0;
+        for(int i = 0;i<32;i++)
+            if(bits[i]%3) res = res | (1<<i);
+        return res;
     }
 };
