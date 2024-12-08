a8d431410669116c249c89a5081c915d9f8cc13a
Sachin <66303835+Sachindebug@users.noreply.github.com>
2024-12-08 22:46:39 +0530

    Time: 0 ms (100%), Space: 19.3 MB (52.7%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,38 +1,50 @@
-class Solution {
+class DisjointSet{
+    vector<int> par;
+    vector<int> size;
 public:
-    void dfs(map<int,vector<int>> &adj,int s,vector<int> &vis)
-    {
-        vis[s]=1;
-        for(auto x:adj[s])
-        {
-            if(!vis[x])
-            {
-                dfs(adj,x,vis);
-            }
+    DisjointSet(int n){
+        size.resize(n+1,0);
+        par.resize(n+1);
+        for(int i = 0; i<=n;i++){
+            par[i] = i;
+            size[i] = 1;
         }
     }
-    int findCircleNum(vector<vector<int>>& arr) {
-        map<int,vector<int>> adj;
-        for(int i=0;i<arr.size();i++)
-        {
-            for(int j=0;j<arr.size();j++)
-            {
-                if(i!=j && arr[i][j]==1)
-                {
-                    adj[i].push_back(j);
+
+    int findPar(int u){
+        if(par[u]==u) return u;
+        return par[u] = findPar(par[u]);
+    }
+
+    void unionBySize(int u, int v){
+        int uPar = findPar(u);
+        int vPar = findPar(v);
+        if(uPar==vPar) return;
+        if(size[uPar]<size[vPar]){
+            size[vPar]+=size[uPar];
+            par[uPar] = vPar; 
+        }
+        else{
+            size[uPar]+=size[vPar];
+            par[vPar] = uPar; 
+        }
+    }
+};
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        DisjointSet ds(n);
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                if(isConnected[i][j]){
+                    ds.unionBySize(i,j);
                 }
             }
         }
-        int n=arr.size();
-        int count=0;
-        vector<int> vis(n,0);
-        for(int i=0;i<n;i++)
-        {
-            if(!vis[i])
-            {
-                count++;
-                dfs(adj,i,vis);
-            }
+        int count = 0;
+        for(int i = 0;i<n;i++){
+            if(ds.findPar(i)==i) count++;
         }
         return count;
     }
