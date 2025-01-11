ec50b7deb2b640fb14d075b460ec6cde4ea526d1
Sachin <66303835+Sachindebug@users.noreply.github.com>
2025-01-11 15:30:06 +0530

    Time: 12 ms (35.31%), Space: 15.2 MB (21.68%) - LeetHub

---
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string s, int k) {
+        int n=s.length();
+        unordered_map<int,int> ump;
+        if(n<k) return false;
+        if(n==k) return true;
+        for(auto x:s) ump[x]++;
+        int odd=0;
+        int even=0;
+        for(auto x:ump)
+        {
+            if(x.second%2) odd++;
+            else even++;
+        }
+        if(odd<=k) return true;
+        return false;
+    }
+};
