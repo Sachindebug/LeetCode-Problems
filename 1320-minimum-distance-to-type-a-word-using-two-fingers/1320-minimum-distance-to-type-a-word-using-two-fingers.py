78866b8a1a2697cadbd42e9aed6740b645f19d47
Sachin <66303835+Sachindebug@users.noreply.github.com>
2026-04-12 10:57:34 +0530

    Time: 203 ms (50.85%), Space: 54 MB (23.73%) - LeetHub

--- 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.py
+++ 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.py
@@ -1,29 +1,19 @@
-from functools import lru_cache
-
 class Solution:
     def minimumDistance(self, word: str) -> int:
-        coords = [(i//6, i%6) for i in range(26)]
+        def moveCost(pos1,pos2):
+            if pos1==-1 or pos2==-1:
+                return 0
+            x1,y1 = pos1//6, pos1%6
+            x2,y2 = pos2//6, pos2%6
+            return abs(x2-x1) + abs(y2-y1)
 
         @lru_cache(None)
-        def dp(pos1, pos2, idx):
-            if idx == len(word):
+        def minDist(pos1,pos2,curr_pos) -> int:
+            if curr_pos >= len(word):
                 return 0
-            if pos1 > pos2:
-                pos1, pos2 = pos2, pos1
-            curr = ord(word[idx]) - 65
-            x, y = coords[curr]
-            if pos1 == -1:
-                cost1 = 0
-            else:
-                x1, y1 = coords[pos1]
-                cost1 = abs(x - x1) + abs(y - y1)
-            move1 = cost1 + dp(curr, pos2, idx + 1)
-            if pos2 == -1:
-                cost2 = 0
-            else:
-                x2, y2 = coords[pos2]
-                cost2 = abs(x - x2) + abs(y - y2)
-            move2 = cost2 + dp(pos1, curr, idx + 1)
-            return min(move1, move2)
-
-        return dp(-1, -1, 0)
+            char_pos = ord(word[curr_pos])- 65
+            move1 = moveCost(char_pos,pos1) + minDist(char_pos,pos2,curr_pos+1)
+            move2 = moveCost(char_pos,pos2) + minDist(pos1,char_pos,curr_pos+1)
+            return min(move1,move2)
+        
+        return minDist(-1,-1,0)
