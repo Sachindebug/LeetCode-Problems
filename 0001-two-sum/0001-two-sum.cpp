f1b1bb335aaedacb177331f6213446238b8bfd15
Sachin <66303835+Sachindebug@users.noreply.github.com>
2026-08-09 09:15:19 +0530

    Time: 2 ms (72.14%), Space: 14.7 MB (57.32%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,11 +1,32 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        map<int,int> ump;
-        for(int i = 0;i<nums.size();i++){
-            if(ump.find(target-nums[i])!=ump.end()) return {ump[target-nums[i]],i};
-            ump[nums[i]] = i;
+std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        int n = nums.size();
+        // Store pairs of {value, original_index}
+        std::vector<std::pair<int, int>> num_with_index(n);
+        for (int i = 0; i < n; i++) {
+            num_with_index[i] = {nums[i], i};
         }
-        return {-1,-1};
+
+        // Sort by value (std::pair sorts by the first element by default)
+        std::sort(num_with_index.begin(), num_with_index.end());
+
+        int left = 0;
+        int right = n - 1;
+
+        while (left < right) {
+            int currentSum = num_with_index[left].first + num_with_index[right].first;
+
+            if (currentSum == target) {
+                // Return original indices
+                return {num_with_index[left].second, num_with_index[right].second};
+            } else if (currentSum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return {};
     }
 };
